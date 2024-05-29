c6eb14488b547303957312e32096c7a31070d646
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-05-29 12:14:54 +0530

    Added: `discord-moderation-bot/src/features/customizableCommands.js`

---
+++ discord-moderation-bot/src/features/customizableCommands.js
@@ -0,0 +1,15 @@
+File: discord-moderation-bot/src/features/customizableCommands.js
+
+const customizableCommands = {
+  customCommand1: function() {
+    // Logic for custom command 1
+  },
+  customCommand2: function() {
+    // Logic for custom command 2
+  },
+  customCommand3: function() {
+    // Logic for custom command 3
+  },
+};
+
+module.exports = customizableCommands;
