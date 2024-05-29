5eb5552a6daf7adfa0bad9d0c69e21440fde3403
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-05-29 12:14:47 +0530

    Added: `discord-moderation-bot/src/features/reportingSystem.js`

---
+++ discord-moderation-bot/src/features/reportingSystem.js
@@ -0,0 +1,12 @@
+File: discord-moderation-bot/src/features/reportingSystem.js
+
+const Discord = require('discord.js');
+
+module.exports = {
+  name: 'reportingSystem',
+  description: 'A reporting system for users to flag suspicious activities to moderators.',
+  execute(message, args) {
+    // Logic for reporting system
+    // Include code here to handle reporting functionality
+  }
+};
